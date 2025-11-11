c9f36eccb8b3ced2673d10254a38401443741db1
Ben Wernsman <ben.wernsman@me.com>
2025-11-11 16:40:49 -0600

    Update withPodfile.ts

--- plugin/src/ios/withPodfile.ts
+++ plugin/src/ios/withPodfile.ts
@@ -2,9 +2,9 @@ import { mergeContents } from "@expo/config-plugins/build/utils/generateCode"
 import { ExportedConfigWithProps, XcodeProject, } from "expo/config-plugins"
 import * as fs from "fs"
 import * as path from "path"
+import { WithExpoIOSWidgetsProps } from ".."
 import { Logging } from "../utils/logger"
 import { getTargetName } from "./withWidgetXCode"
-import { WithExpoIOSWidgetsProps } from ".."
 
 export const withPodfile = (config: ExportedConfigWithProps<XcodeProject>, options: WithExpoIOSWidgetsProps) => {
   const targetName = `${getTargetName(config, options)}`
@@ -35,10 +35,22 @@ end
   const withAppExtFix = mergeContents({
     tag: "app_ext_fix",
     src: podFileContent,
-    newSrc: `
-        config.build_settings['APPLICATION_EXTENSION_API_ONLY'] = '${AppExtValue}'
-        `,
-    anchor: /resource_bundle_target.build_configurations.each do \|config\|/,
+    newSrc: `    installer.target_installation_results.pod_target_installation_results
+      .each do |pod_name, target_installation_result|
+      target_installation_result.resource_bundle_targets.each do |resource_bundle_target|
+        resource_bundle_target.build_configurations.each do |config|
+          config.build_settings['APPLICATION_EXTENSION_API_ONLY'] = 'NO'
+        end
+      end
+    end`,
+    anchor: `  post_install do |installer|
+    react_native_post_install(
+      installer,
+      config[:reactNativePath],
+      :mac_catalyst_enabled => false,
+      :ccache_enabled => podfile_properties['apple.ccacheEnabled'] == 'true',
+    )
+  end`,
     offset: 1,
     comment: "#",
   })
@@ -46,10 +58,10 @@ end
   const withAppExtFixPt2 = mergeContents({
     tag: 'fix2',
     src: withAppExtFix.contents,
-    newSrc: ` installer.pods_project.targets.each do |target|
+    newSrc: `    installer.pods_project.targets.each do |target|
         target.build_configurations.each do |config|
           config.build_settings['CODE_SIGNING_ALLOWED'] = 'NO'
-          config.build_settings['APPLICATION_EXTENSION_API_ONLY'] = '${AppExtValue}'        
+          config.build_settings['APPLICATION_EXTENSION_API_ONLY'] = 'NO'
         end
       end`,
     anchor: /post_install do \|installer\|/,
